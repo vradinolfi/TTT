8022e7719e9ac1b5787033fbfbf909e088ed4fc7
Vincent Adinolfi <vincentadinolfi@Vincents-MacBook-Pro.local>
2015-09-22 09:03:57 -0500

    more login stuff

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <? include "base.php"; ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html>
     <head>
         
@@ -24,12 +24,56 @@
     <body>
         <header>
         
-            <h1>Hello, world.</h1>
+            <h1>Transmute Tournament Tracker</h1>
             
         </header>
         <main>
             
+        <?
+        if (!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username'])) {
+            // let the user access the main page
+        ?>
             
+            <h1>Member Area</h1>
+            
+            <p>Thanks for logging in! You are <code><?=$_SESSION['Username']?></code> and your email address is <code><?=$_SESSION['EmailAddress']?></code>.</p>
+            
+        <?php     
+        }
+
+        elseif(!empty($_POST['username']) && !empty($_POST['password'])) {
+            // let the user login
+            
+            $username = mysql_real_escape_string($_POST['username']);
+            $password = md5(mysql_real_escape_string($_POST['password']));
+            
+            $checklogin = mysql_query("SELECT * FROM users WHERE Username = '".$username."' AND Password = '".$password."'");
+            
+            if(mysql_num_rows($checklogin) == 1)
+            {
+                $row = mysql_fetch_array($checklogin);
+                $email = $row['EmailAddress'];
+                
+                $_SESSION['Username'] = $username;
+                $_SESSION['EmailAddress'] = $email;
+                $_SESSION['LoggedIn'] = 1;
+                
+                echo "<h1>Success</h1>";
+                echo "<p>We are now resirecting you to the member area.</p>";
+                echo "<meta http-equiv='refresh' content='=2;index.php' />";
+            }
+            else
+            {
+                echo "<h1>Error</h1>";
+                echo "<p>Sorry, your account could not be found. Please <a href=\"index.php\">click here to try again</a>.</p>";
+            }
+        }
+
+        else {
+            // display the login form
+        }
+
+        ?>
             
         </main>
         <footer>
